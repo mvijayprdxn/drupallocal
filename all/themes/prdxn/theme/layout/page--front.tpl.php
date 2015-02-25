2334f91e265abce1025f4173e753e31c7cb1f880
mvijayprdxn <vijay.mishra.prdxn@gmail.com>
2015-02-25 20:49:46 +0530

    Created structure of home page with styling

--- all/themes/prdxn/theme/layout/page--front.tpl.php
+++ all/themes/prdxn/theme/layout/page--front.tpl.php
@@ -3,7 +3,12 @@
 <div class="row">
   <div class="col-lg-12">
     <div class="main-container container-fluid">
-  <div class="animated-background"> 
+      <div class="animated-background">
+	
+
+
+ <?php print views_embed_view('homepage','block'); ?>
+ 
     <h2>PRDXN</h2>
     <h3>WE ARE DRUPAL DEVELOPERS</h3>
   </div>
