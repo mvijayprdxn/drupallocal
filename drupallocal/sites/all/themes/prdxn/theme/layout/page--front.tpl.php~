83c8d923de17091ec0a8b7fbdfc69bc05f247c6a
mvijayprdxn <vijay.mishra.prdxn@gmail.com>
2015-03-12 20:52:06 +0530

    Pushing to acquia

---
+++ drupallocal/sites/all/themes/prdxn/theme/layout/page--front.tpl.php~
@@ -0,0 +1,17 @@
+
+<?php @include 'header.tpl.php';?>
+<div class="row">
+  <div class="col-lg-12">
+    <div class="main-container container-fluid">
+      <div class="animated-background">
+	
+
+
+ <?php print views_embed_view('homepage','block'); ?>
+ 
+    <h2>PRDXN</h2>
+    <h3>WE ARE DRUPAL DEVELOPERS</h3>
+  </div>
+</div>
+  </div>
+</div>
---
+++ drupallocal/sites/gruntfile.js
@@ -0,0 +1,94 @@
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    watch: {
+      src: {
+        files: ['**/*.scss','**/*.css', '**/*.php'],
+          tasks: ['sass', 'jshint', 'concat', 'uglify', 'cssmin', 'concat_css']
+      },
+      options: {
+        livereload: true,
+      },
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'all/themes/prdxn/css/header.css': 'all/themes/prdxn/sass/header.scss',
+	       'all/themes/prdxn/css/homepage.css': 'all/themes/prdxn/sass/homepage.scss'       // 'destination': 'source'
+        }
+      }
+    },
+
+    jshint: {
+    options: {
+      curly: true,
+      eqeqeq: true,
+      eqnull: true,
+      browser: true,
+      camelcase: true,
+      funcscope: true,
+      globals: {
+        jQuery: true
+      }
+    }
+  },
+
+
+  concat: {
+    options: {
+      separator: ' ',
+    },
+    dist: {
+      src: 'all/themes/prdxn/js/customjs/*.js',
+      dest: 'all/themes/prdxn/prod/js/script.js',
+    },
+  },
+
+  uglify: {
+    my_target: {
+      files: {
+        'all/themes/prdxn/prod/js/script.min.js': 'all/themes/prdxn/prod/js/script.js'
+      }
+    }
+  },
+
+  concat_css: {
+    options: {
+      // Task-specific options go here.
+    },
+    all: {
+      src: ["all/themes/prdxn/css/*.css"],
+      dest: "all/themes/prdxn/prod/css/styles.css"
+    },
+  },
+
+  cssmin: {
+  options: {
+    shorthandCompacting: false,
+    roundingPrecision: -1
+  },
+  target: {
+    files: {
+      'all/themes/prdxn/prod/css/styles.min.css': 'all/themes/prdxn/prod/css/styles.css'
+    }
+  }
+}
+
+
+
+});
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['jshint', 'watch', 'sass', 'concat', 'uglify', 'cssmin', 'concat_css']);
+
+};
